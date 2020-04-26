6cba26d23a71da35835673b083ff42442a51b0f7
liujx <liujiaxing37@outlook.com>
2020-04-26 14:51:24 +0800

    将thread项目改为maven构建结构项目

---
+++ thread/src/test/java/com/iskech/thread/ThreadApplicationTests.java
@@ -0,0 +1,11 @@
+package com.iskech.thread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class ThreadApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}
